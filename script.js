6dc3b7ab2328c462212be3fe7c6112f62d2c52db
moss-3c <117598547+moss-cc@users.noreply.github.com>
2025-06-09 22:53:22 -0700

    Add button UI for the game
    Don't have to play in console anymore

--- script.js
+++ script.js
@@ -13,32 +13,34 @@ function getComputerChoice () {
             return "scissors";
             break;
         default:
-            return "error: choice is "+choice;
+            return "error: computer chose: "+choice;
     }
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("rock, paper, or scissors?");
+    let humanChoice = prompt("rock, paper, or scissors?"); //console
     return humanChoice.toLowerCase().trim();
 }
 
 function playRound (humanChoice, computerChoice) {
 
-    console.log("You chose: "+humanChoice+". Computer chose: "+computerChoice);
+    humanChoice = humanChoice.toLowerCase();
+    choiceDisplay.textContent = "You chose: "+ humanChoice +". Computer chose: "+computerChoice;
 
+    roundsPlayed++;
     switch (humanChoice) {
         case computerChoice:
-            console.log("Tie! Both players chose", humanChoice);
+            matchResult.textContent = "Tie! Both players chose " + humanChoice;
             break;
         case "rock":
             switch (computerChoice) {
                 case "scissors":
                     humanScore++;
-                    console.log("You win! Rock beats scissors")
+                    matchResult.textContent = "You win! Rock beats scissors";
                     break;
                 case "paper":
                     computerScore++;
-                    console.log("You lose! Paper beats rock")
+                    matchResult.textContent = "You lose! Paper beats rock";
                     break;
             }
             break;
@@ -46,11 +48,11 @@ function playRound (humanChoice, computerChoice) {
             switch (computerChoice) {
                 case "rock":
                     humanScore++;
-                    console.log("You win! Paper beats rock")
+                    matchResult.textContent = "You win! Paper beats rock";
                     break;
                 case "scissors":
                     computerScore++;
-                    console.log("You lose! Scissors beats paper")
+                    matchResult.textContent = "You lose! Scissors beats paper";
                     break;
             }
             break;
@@ -58,17 +60,16 @@ function playRound (humanChoice, computerChoice) {
             switch (computerChoice) {
                 case "rock":
                     computerScore++;
-                    console.log("You lose! Rock beats scissors");
+                    matchResult.textContent = "You lose! Rock beats scissors";
                     break;
                 case "paper":
                     humanScore++;
-                    console.log("You win! Paper beats scissors");
+                    matchResult.textContent = "You win! Paper beats scissors";
                     break;
             }
             break;
         default:
-            console.log("smth went wrong..");
-            console.log("computer", computerChoice, "human", humanChoice);
+            matchResult.textContent = "something went wrong while computing score..";
     }
     }
 
@@ -77,23 +78,45 @@ function playGame () {
     let humanChoice, computerChoice;
 
     //play 5 rounds
-    for (let i = 0; i < 5; i++) {
-        humanChoice = getHumanChoice();
-        computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
+    // for (let i = 0; i < 5; i++) {
+    //     humanChoice = getHumanChoice();
+    //     computerChoice = getComputerChoice();
+    //     playRound(humanChoice, computerChoice);
+    // }
+
+
+}
+
+function declareWinner () {
+    if (humanScore === computerScore)
+        return "Tie! Your score: "+ humanScore + ". Computer's score: "+computerScore;
+    else if (humanScore > computerScore)    
+        return "You win! Your score: "+ humanScore + ". Computer's score: "+computerScore;
+    else {
+        return "You lose! Your score: "+ humanScore + ". Computer's score: "+computerScore;
     }
-
-    //declare winner
-    humanScore === computerScore  ? console.log("Tie! Your score: "+ humanScore + ". Computer's score: "+computerScore):
-    humanScore > computerScore    ? console.log("You win! You score: "+ humanScore + ". Computer's score: "+computerScore):
-    console.log("You lose! Your score: "+ humanScore + ". Computer's score: "+computerScore);
-
 }
 
 let humanScore = 0;
 let computerScore = 0;
+let roundsPlayed = 0;
+const currentScore = document.querySelector(".currentScore");
+const matchResult = document.querySelector(".matchResult");
+const choiceDisplay = document.querySelector(".choiceDisplay");
+const winnerDisplay = document.querySelector(".winnerDisplay");
 
-playGame(); //5 rounds
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        playRound(button.textContent, getComputerChoice());
+        currentScore.textContent = "Your score: " + humanScore + ". Computer's score: " + computerScore;
+        if (roundsPlayed >= 5) {   //declare winner
+            winnerDisplay.textContent = declareWinner();
+        }
+    })
+});
+
+
 
 
 
